62d34a2108df95f5e5a9df5b1f2fc852b10397b2
JacksiroKe <Jacksiroke@gmail.com>
2020-10-17 17:29:15 +0300

    added sample app

---
+++ sample/MdiTabCtrlSample/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace MdiTabCtrlSample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new JacksiroKe.MdiTabCtrl.TabControl();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.MenuRenderer = null;
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.Size = new System.Drawing.Size(800, 450);
+            this.tabControl1.TabCloseButtonImage = null;
+            this.tabControl1.TabCloseButtonImageDisabled = null;
+            this.tabControl1.TabCloseButtonImageHot = null;
+            this.tabControl1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private JacksiroKe.MdiTabCtrl.TabControl tabControl1;
+    }
+}
+
---
+++ sample/MdiTabCtrlSample/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MdiTabCtrlSample
+{
+    public partial class Form1 : Form
+    {
+        private int tabcount;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            NewTab();
+            NewTab();
+            NewTab();
+            tabControl1.TabPages[1].Select(); //select a tab page of choice
+        }
+
+        public void NewTab()
+        {
+            Form cform = new ChildForm(); //call an instance of a class you want to show in a tab
+            tabcount = tabcount + 1;
+            cform.Text = "This is Tab " + tabcount; //Declare text for this tab
+            tabControl1.TabPages.Add(cform);
+        }
+    }
+}
